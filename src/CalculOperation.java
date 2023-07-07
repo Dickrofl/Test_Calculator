dad93354fb2b84c82201205a1ffdc3dc9796bddb
Dikrof <annanedavnyaya@yandex.ru>
2023-07-07 13:01:54 +0300

    Add check input range

--- src/CalculOperation.java
+++ src/CalculOperation.java
@@ -3,6 +3,10 @@ public class CalculOperation {
         String firstNumber = inputMas[0];
         String operator = inputMas[1];
         String secondNumber = inputMas[2];
+        if (Integer.parseInt(firstNumber) > 10 || Integer.parseInt(secondNumber) > 10 ||
+                Integer.parseInt(firstNumber) < 1 || Integer.parseInt(secondNumber) < 1) {
+            throw new IllegalArgumentException("//т.к. используйте числа от 1 до 10 ");
+        }
         int result = 0;
         if (operator.equals("+")) {
             result = Integer.parseInt(firstNumber) + Integer.parseInt(secondNumber);
